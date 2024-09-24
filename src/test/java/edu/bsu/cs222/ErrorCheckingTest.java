6aed0a8a1ff175dc782abc3df570526ab4b3d1d9
ColeCurtis27 <curtisco2004@gmail.com>
2024-09-24 13:39:26 -0400

    Add working tests for ErrorChecking Class.

---
+++ src/test/java/edu/bsu/cs222/ErrorCheckingTest.java
@@ -0,0 +1,21 @@
+package edu.bsu.cs222;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class ErrorCheckingTest {
+    @Test
+    public void testNoArticleExists() throws IOException {
+        System.out.println("Test 1:");
+        String userInput = "dfasfblaknflkasnklfnasklfndfnlkdsnflkdsnfl";
+        ErrorChecking.noArticleExists(userInput);
+    }
+    @Test
+    public void testNoArticleExistsLamp() throws IOException {
+        String userInput = "Lamp";
+        System.out.println("Test 2:");
+        ErrorChecking.noArticleExists(userInput);
+    }
+}
