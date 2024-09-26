fd77ef68b0dd90cb2284226d364ba144162625ee
LleytonParsons <168110045+LleytonParsons@users.noreply.github.com>
2024-09-26 10:33:14 -0400

    Add example code for JavaFX implementation. Use as baseline for GUI building.

--- src/main/java/edu/bsu/cs222/ConsoleMain.java
+++ src/main/java/edu/bsu/cs222/ConsoleMain.java
@@ -2,9 +2,17 @@ package edu.bsu.cs222;
 
 import java.io.IOException;
 import java.util.Scanner;
+import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class ConsoleMain {
-    public static void main(String[] args) {
+public class ConsoleMain extends Application {
+    /*public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the name of the wikipedia article you wish to access: ");
         String userInput = scanner.nextLine();
@@ -17,5 +25,22 @@ public class ConsoleMain {
         } catch (IOException ioException) {
             System.err.println("Network Connection Problem: " + ioException.getMessage());
         }
+    }*/
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        VBox parent = new VBox();
+        parent.getChildren().add(new Label("Wikipedia Article Revision Finder"));
+
+        HBox urlArea = new HBox(new Label("Please enter name of the Wikipedia article you wish to access:"));
+        TextField textField = new TextField();
+        urlArea.getChildren().add(textField);
+        parent.getChildren().add(urlArea);
+
+        Button button = new Button("Get Revisions");
+        button.setOnAction(actionEvent -> System.out.println("I would access " + textField.getText() + " here"));
+        parent.getChildren().add(button);
+
+        primaryStage.setScene(new Scene(parent));
+        primaryStage.show();
     }
 }
