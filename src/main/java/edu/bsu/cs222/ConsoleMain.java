7857887d357fa58e3befaf32c2f474ce67844d0a
LleytonParsons <168110045+LleytonParsons@users.noreply.github.com>
2024-09-27 14:30:23 -0400

    Add independent error dialog message boxes for a noInput error and a network connection error. Refractor code to be how it is a commit or two ago with functioning GUI methods and functions.

--- src/main/java/edu/bsu/cs222/ConsoleMain.java
+++ src/main/java/edu/bsu/cs222/ConsoleMain.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.*;
 import javafx.application.Application;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
@@ -32,7 +33,7 @@ public class ConsoleMain extends Application{
         }
     }*/
     @Override
-    public void start(Stage primaryStage) throws IOException{
+    public void start(Stage primaryStage){
         VBox parent = new VBox();
         parent.getChildren().add(new Label("Wikipedia Article Revision Finder"));
 
@@ -41,30 +42,40 @@ public class ConsoleMain extends Application{
         urlArea.getChildren().add(textField);
         parent.getChildren().add(urlArea);
 
-        HBox revisionOutput = new HBox();
-
-        Button button = new Button("Get Revisions");
+        Button button = new Button("Get 15 Most Recent Revisions");
         button.setOnAction(actionEvent -> {
+
             try {
-                revisionOutput.getChildren().clear();
                 String userInput = textField.getText();
                 Label noArticle = new Label(ErrorChecking.noArticleExists(userInput));
-                revisionOutput.getChildren().add(noArticle);
+                parent.getChildren().add(noArticle);
                 Label redirect = new Label(ErrorChecking.redirectPrintOut(userInput));
-                revisionOutput.getChildren().add(redirect);
-                ArrayList<String> revisions = new ArrayList<>(FormatOutput.formatOutput(userInput));
-                for (String revision : revisions) {
-                    Label revisionLabel = (new Label(revision));
-                    revisionOutput.getChildren().add(revisionLabel);
+                parent.getChildren().add(redirect);
+                if(userInput.isEmpty()){
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Input Error");
+                    alert.setHeaderText(null);
+                    alert.setContentText(ErrorChecking.noInputError());
+                    alert.showAndWait();
+                }
+                else{
+                    ArrayList<String> revisions = new ArrayList<>(FormatOutput.formatOutput(userInput));
+                    for (String revision : revisions) {
+                        Label revisionLabel = (new Label(revision));
+                        parent.getChildren().add(revisionLabel);
+                    }
                 }
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Network Error");
+                alert.setHeaderText(null);
+                alert.setContentText("Network Connection Problem: " + e.getMessage());
+                alert.showAndWait();
             }
         });
         parent.getChildren().add(button);
 
         primaryStage.setScene(new Scene(parent));
-        primaryStage.setScene(new Scene(revisionOutput));
         primaryStage.show();
     }
 }
