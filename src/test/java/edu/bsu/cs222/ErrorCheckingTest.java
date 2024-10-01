013714f47b86a2022ea97413409302730b2faa4e
LleytonParsons <168110045+LleytonParsons@users.noreply.github.com>
2024-10-01 14:37:11 -0400

    Delete a redundant testNoArticleExistsLamp() function in ErrorCheckingTest.java, as testNoArticleExists() fails successfully with an input of "Lamp" instead of an input of gibberish.

--- src/test/java/edu/bsu/cs222/ErrorCheckingTest.java
+++ src/test/java/edu/bsu/cs222/ErrorCheckingTest.java
@@ -8,15 +8,8 @@ import java.io.IOException;
 public class ErrorCheckingTest {
     @Test
     public void testNoArticleExists() throws IOException {
-        System.out.println("Test 1:");
         String userInput = "dfasfblaknflkasnklfnasklfndfnlkdsnflkdsnfl";
         String errorResult = ErrorChecking.noArticleExists(userInput);
         assertEquals("No article exists for that input.",errorResult);
-    }
-    @Test
-    public void testNoArticleExistsLamp() throws IOException {
-        String userInput = "Lamp";
-        System.out.println("Test 2:");
-        ErrorChecking.noArticleExists(userInput);
     }
 }
